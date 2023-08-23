9ce17405c997a3af6043cfcdada53b5099ebe381
yangjijian <yangjijian@sensorsdata.cn>
2023-08-23 19:04:33 +0800

    feat: use class base view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,3 +20,15 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    # query results, default is object_list if don't specify any context_object_name
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'  # default is blog/post_list.html
+    # default page result is page_obj
