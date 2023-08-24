9503665afa612b1a65f43ca83beaae5a28f6232c
yangjijian <yangjijian@sensorsdata.cn>
2023-08-24 12:01:52 +0800

    feat: support email share

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     # re_path(r'^$', views.post_list, name='post_list'),
     re_path(r'^$', views.PostListView.as_view(), name='post_list'),
+    re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
     re_path(
         r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -20,6 +22,23 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    send = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} ({cd['email']}) recommends you reading '{post.title}'"
+            message = f"Read '{post.title}' at {post_url}\n\n{cd['name']}'s recomments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            send = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'send': send})
 
 
 from django.views.generic import ListView
